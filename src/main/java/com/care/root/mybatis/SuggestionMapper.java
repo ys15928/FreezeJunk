370825f044e3b556f0c15d55c96cde7a5a6c486e
KJH <wnsgh9978@naver.com>
2022-02-04 13:22:38 +0900

    내 건의사항 리스트 추가

--- src/main/java/com/care/root/mybatis/SuggestionMapper.java
+++ src/main/java/com/care/root/mybatis/SuggestionMapper.java
@@ -9,7 +9,9 @@ import com.care.root.suggestion.SuggestionDTO;
 public interface SuggestionMapper {
 	public int write(SuggestionDTO dto);
 	public ArrayList<SuggestionDTO> allList(@Param("s")int start, @Param("e")int end, @Param("search")String search);
+	public ArrayList<SuggestionDTO> myList(@Param("s")int start, @Param("e")int end, @Param("search")String search, @Param("id") String id);
 	public int allListCount(String search);
+	public int myListCount(@Param("id")String id, @Param("search")String search);
 	public SuggestionDTO info(int num);
 	public int answer(SuggestionDTO dto);
 	public int update(SuggestionDTO dto);
