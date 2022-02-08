33c572dc2e07e2bb859dfe1cd990dccc0d023c4b
KJH <wnsgh9978@naver.com>
2022-02-08 13:30:11 +0900

    kjh Freezejunk 검색 페이지 3개 화면구현

---
+++ src/main/java/com/care/root/freezejunk/FreezejunkController.java
@@ -0,0 +1,22 @@
+package com.care.root.freezejunk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FreezejunkController {
+	@RequestMapping(value="main/freezejunk-1")
+	public String urlSearch() {
+		return "freezejunk/urlSearch";
+	}
+	
+	@RequestMapping(value="main/freezejunk-2")
+	public String keywordsSearch() {
+		return "freezejunk/keywordsSearch";
+	}
+	
+	@RequestMapping(value="main/freezejunk-3")
+	public String userSearch() {
+		return "freezejunk/userSearch";
+	}
+}
